0bb046833b88f10c33fd4f88db6e5563a060e8a3
MBRSL <mbrsl@gmail.com>
2023-10-04 21:06:54 +0900

    Separate Main for multiple instance training

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,23 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Main : MonoBehaviour
 {
-    #region Editor data
-    [SerializeField] private GameView _gameView;
-    #endregion
-    
-    private GameManager _gameManager;
-
-    #region Unity functions
     void Start()
     {
         Application.targetFrameRate = 60;
-        _gameManager = new GameManager(_gameView);
     }
-
-    void Update()
-    {
-        _gameManager.Update();
-    }
-    #endregion
 }
